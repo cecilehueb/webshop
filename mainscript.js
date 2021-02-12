4bce05bd378453ba680e0afe82771e3278e73cb7
cecilehueb <cecilehubner@gmail.com>
2021-02-12 20:19:50 +0100

    add index style and script

---
+++ mainscript.js
@@ -0,0 +1,18 @@
+let photo1 = document.getElementById("photo1");
+let photo2 = document.getElementById("photo2");
+let descrip1 = document.getElementById("descrip1");
+let title = document.getElementById("title");
+let productList = document.getElementById("productList");
+let fillingimage = documet.getElementById("fillingimage");
+
+
+function textappear1() {
+    photo1.style.display = "none";
+    descrip1.innerHTML = "I’m looking at a series of photographs of the rear-end of trucks that I took on a snowy day with Cecile. I’m looking particularly at this red truck with a tomato red coat of paint. An infographic of an arrow slices through the logo. The arrow flies from right to left. The fleeting has flown through the stem of the ’S’, right through the negative space of the ‘O’. finally piercing it’s arrowhead to the start of the word the ‘V’. The word when read from left right reads ‘Vos’ or Fox in English.";
+    descrip1.style.display = "block";
+    title.style.color = "tomato";
+}
+
+
+
+
