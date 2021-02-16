30ac5258e6af5f92ee47f2c577ef1289682aeb0b
cecilehueb <cecilehubner@gmail.com>
2021-02-16 12:21:22 +0100

    new version style script index

--- mainscript.js
+++ mainscript.js
@@ -1,16 +1,24 @@
 let photo1 = document.getElementById("photo1");
 let photo2 = document.getElementById("photo2");
+let photo3 = document.getElementById("photo3");
+let photo4 = document.getElementById("photo4");
+
+let ocean = document.getElementById("ocean");
 let descrip1 = document.getElementById("descrip1");
 let title = document.getElementById("title");
 let productList = document.getElementById("productList");
-let fillingimage = documet.getElementById("fillingimage");
+let fillingimage = document.getElementById("fillingimage");
 
 
 function textappear1() {
-    photo1.style.display = "none";
-    descrip1.innerHTML = "I’m looking at a series of photographs of the rear-end of trucks that I took on a snowy day with Cecile. I’m looking particularly at this red truck with a tomato red coat of paint. An infographic of an arrow slices through the logo. The arrow flies from right to left. The fleeting has flown through the stem of the ’S’, right through the negative space of the ‘O’. finally piercing it’s arrowhead to the start of the word the ‘V’. The word when read from left right reads ‘Vos’ or Fox in English.";
-    descrip1.style.display = "block";
-    title.style.color = "tomato";
+    photo1.src = "img/alter/alter1.jpg";
+    photo2.src = "img/alter/1.png";
+    photo3.src = "img/alter/alter2.jpg";
+    photo4.src = "img/alter/alter4.png";
+}
+
+function textappear2() {
+    ocean.style.display = "block";
 }
 
 
