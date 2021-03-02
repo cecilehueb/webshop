bb357240e6259bd94b2b5d37f68c0b476a0aaa96
cecilehueb <cecilehubner@gmail.com>
2021-03-02 15:48:45 +0100

    website status txttalk1

--- mainscript.js
+++ mainscript.js
@@ -1,7 +1,4 @@
-let photo1 = document.getElementById("photo1");
-let photo2 = document.getElementById("photo2");
-let photo3 = document.getElementById("photo3");
-let photo4 = document.getElementById("photo4");
+
 
 let ocean = document.getElementById("ocean");
 let descrip1 = document.getElementById("descrip1");
@@ -29,7 +26,47 @@ function myFunction() {
   }
 }
 
+// makes background of imagies visible 
+let photo1 = document.getElementById("photo1");
+let photo2 = document.getElementById("photo2");
+let photo3 = document.getElementById("photo3");
+let photo4 = document.getElementById("photo4");
 
+photo1.addEventListener("mouseenter", () => {
+  photo1.parentElement.style.background = "rgb(10,73,32);";
+  photo1.parentElement.style.background = "linear-gradient(0deg, rgba(10,73,32,0.7189250700280112) 5%, rgba(128,218,32,0.6573004201680672) 74%)";
+})
+
+photo1.addEventListener("mouseleave", () => {
+  photo1.parentElement.style.background = "none";
+})
+
+photo2.addEventListener("mouseenter", () => {
+  photo2.parentElement.style.background = "rgb(47,51,106)";
+  photo2.parentElement.style.background = "linear-gradient(0deg, rgba(47,51,106,0.7833508403361344) 0%, rgba(118,126,138,1) 100%)";
+})
+
+photo2.addEventListener("mouseleave", () => {
+  photo2.parentElement.style.background = "none";
+})
+
+photo3.addEventListener("mouseenter", () => {
+  photo3.parentElement.style.background = "rgb(4,158,43);";
+  photo3.parentElement.style.background = "linear-gradient(0deg, rgba(4,158,43,0.7749474789915967) 5%, rgba(64,98,91,0.7469362745098039) 74%)";
+})
+
+photo3.addEventListener("mouseleave", () => {
+  photo3.parentElement.style.background = "none";
+})
+
+photo4.addEventListener("mouseenter", () => {
+  photo4.parentElement.style.background = "rgb(6,36,242);";
+  photo4.parentElement.style.background = "linear-gradient(0deg, rgba(6,36,242,0.7749474789915967) 5%, rgba(34,250,209,0.9374124649859944) 82%)";
+})
+
+photo4.addEventListener("mouseleave", () => {
+  photo4.parentElement.style.background = "none";
+})
 
 // this function adds to the location description list 
 
@@ -42,17 +79,26 @@ function addLocation(input) {
 
 // these eventlisteners listen for bubble up events and connect text to it
 
-const product = document.querySelector('.products-list');
-product.addEventListener("mouseenter", function(){addLocation(" > you clicked a product")}, false);
+photo1.addEventListener("mouseenter", function(){addLocation(" > the grass is layered like a cake")}, false);
+
+photo2.addEventListener("mouseenter", function(){addLocation(" > you clicked a product")}, false);
+
+photo3.addEventListener("mouseenter", function(){addLocation(" > within 20 minutes the sky goes black")}, false);
+
+photo4.addEventListener("mouseenter", function(){addLocation(" > you clicked a product")}, false);
+
+
+const searchbar = document.querySelector(".searchbar");
+searchbar.addEventListener("mouseenter", function(){addLocation(" > you are rushing over the highway")}, false);
 
 const firstButton = document.querySelector('.nav-button:nth-child(1)');
-firstButton.addEventListener("mouseenter", function(){addLocation("  > you are walking next to the highway ")}, false);
+firstButton.addEventListener("mouseenter", function(){addLocation("  > the sky is an etarnal tint of ungraspable blue ")}, false);
 
 const secondButton = document.querySelector('.nav-button:nth-child(2)');
-secondButton.addEventListener("mouseenter", function(){addLocation("  > if you look up, right now, the sky is blue blue blue ")}, false);
+secondButton.addEventListener("mouseenter", function(){addLocation("  > do you see the building? ")}, false);
 
 const thirdButton = document.querySelector('.nav-button:nth-child(3)');
-thirdButton.addEventListener("mouseenter", function(){addLocation("  > it is an ungraspable type of blue, you dont know where the sky will end ")}, false);
+thirdButton.addEventListener("mouseenter", function(){addLocation("  >  you are offered a 24h surface")}, false);
 
 // loader
 
